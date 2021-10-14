aaa0e12db34da678a213db4f97e41beb80803193
Băbuță Luminița <70962140+hopeniitssaa@users.noreply.github.com>
2021-10-14 19:45:29 +0200

    Add files via upload

--- ksks2.py
+++ ksks2.py
@@ -1,10 +1,30 @@
-s="A B C D E F H I J K L M N O P" 
+s=str(input("Scrie un sir"))
 s1=""
 s2=""
 s3=""
+
+print("Sirul initial este ", s)
+
+
 for i in s:
+    if ((ord(i)>64) and (ord(i)<90)):
         a=chr(ord(i)+1)
         s1+=a
-        b=s1.replace('!', ' ')
-        x=b.replace('[', 'A')
-print('Primul sir este', x)
+    else:
+        s1+=i      
+
+print("Primul sir este", s1)
+
+
+s2=s1
+for i in s:
+    b=s2.replace(("Z"), ("A"))
+
+print("Al doilea sir este", b) 
+
+
+s3=b
+for i in s:
+    c=l3.replace(' ','-')
+
+print("Al treilea sir este", c)
